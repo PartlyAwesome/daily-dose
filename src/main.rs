e574b79b3bc4e73224c6b06b34d382c6c0282cc6
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2024-11-18 04:17:39 +0000

    Use clap to parse token from env or command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+/// A bot that posts a video daily
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Your discord token
+    #[arg(required = true, short, long, env = "DD_TOKEN")]
+    token: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let _args = Args::parse();
+    println!("we here");
 }
