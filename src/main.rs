83c09442737cff1129490fe70b6446910bbf721f
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2024-11-19 03:14:34 +0000

    Implement !ping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use serenity::{
+    all::{Context, EventHandler, GatewayIntents, Message, Ready},
+    async_trait, Client,
+};
 
 /// A bot that posts a video daily
 #[derive(Parser, Debug)]
@@ -9,8 +13,37 @@ struct Args {
     token: String,
 }
 
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected", ready.user.name);
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let _args = Args::parse();
-    println!("we here");
+    let args = Args::parse();
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&args.token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
